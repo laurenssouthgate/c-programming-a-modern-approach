c6506fea151f47e53ffd30ca2dd5188a92c399b0
Laurens Southgate <laurenssouthgate@gmail.com>
2025-07-03 18:27:35 +0100

    some more exercises

---
+++ chapter-4/exercises/02/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int j = 3;
+	int i = 7;
+
+	printf("%d %d", (-i)/j, - (i/j));
+
+	return 0;
+}
---
+++ chapter-4/exercises/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 8;
+	int j = 5;
+
+	printf("%d\n", i / j);
+	printf("%d\n", -i / j);
+	printf("%d\n", i / -j);
+	printf("%d\n", -i / -j);
+
+	return 0;
+}
+	
